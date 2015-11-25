f31c67b780ecaa7d1523911227e5e4101f86df89
andela-oogunjimi <opeyemi.ogunjimi@andela.com>
2015-11-25 12:25:27 +0000

    [Potato-ORM] Set-up Connections.

---
+++ src/Connections/PgSqlConnection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Opeyemiabiodun\PotatoORM\Connections;
+
+use PDO;
+use PDOException;
+
+final class PgSqlConnection extends Connection
+{
+    protected function create()
+    {
+        $this->useDbEnv();
+
+        $dsn = 'pgsql:host='.$this->_host
+                .';port='.$this->_port
+                .';dbname='.$this->_database
+                .';user='.$this->_username
+                .';password='.$this->_password;
+
+        try {
+            $this->_pdo = new PDO($dsn);
+        } catch (PDOException $e) {
+        }
+    }
+}
