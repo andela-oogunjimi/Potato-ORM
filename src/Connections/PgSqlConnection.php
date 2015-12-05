7d3ca7d7535c3e01f90de0826725e5b3b13fda0f
andela-oogunjimi <opeyemi.ogunjimi@andela.com>
2015-12-05 05:07:14 +0000

    [WIP][Potato-ORM] Make updates to Models and Connections.

--- src/Connections/PgSqlConnection.php
+++ src/Connections/PgSqlConnection.php
@@ -8,7 +8,7 @@ use Opeyemiabiodun\PotatoORM\Connections\Connection;
 
 final class PgSqlConnection extends Connection
 {
-    protected function create()
+    protected function connect()
     {
         $this->useDbEnv();
 
@@ -23,4 +23,29 @@ final class PgSqlConnection extends Connection
         } catch (PDOException $e) {
         }
     }
+
+    public function getColumns($table)
+    {
+        return $this->getPdo()->query("SELECT COLUMN_NAME
+                                        FROM {$this->_database}.INFORMATION_SCHEMA.COLUMNS
+                                        WHERE TABLE_NAME = N{$table}")->fetchAll();
+    }
+
+    public function getPrimaryKey($table)
+    {
+        return $this->getPdo()->query("SELECT KU.table_name as tablename,column_name as primarykeycolumn
+                                        FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS TC
+                                        INNER JOIN
+                                        INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS KU
+                                        ON TC.CONSTRAINT_TYPE = 'PRIMARY KEY' AND
+                                        TC.CONSTRAINT_NAME = KU.CONSTRAINT_NAME
+                                        and ku.table_name='{$table}'
+                                        ORDER BY KU.TABLE_NAME, KU.ORDINAL_POSITION;")->fetchAll()[0][$table];
+    }
 }
+
+
+
+
+
+
