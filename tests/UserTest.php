1fc2691fe94f4bad0bff8894822ee8267780af03
andela-oogunjimi <opeyemi.ogunjimi@andela.com>
2015-12-10 10:03:33 +0000

    [Potato-ORM] Change design

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -2,11 +2,12 @@
 
 namespace Opeyemiabiodun\PotatoORM\Test;
 
+use stdClass;
 use Opeyemiabiodun\PotatoORM\Models\User;
 
 class UserTest extends \PHPUnit_Framework_TestCase
 {
-	public function testCreateUser()
+	public function testUserInstance()
 	{
 		/*
 		CREATE TABLE "User-table"
@@ -29,6 +30,8 @@ class UserTest extends \PHPUnit_Framework_TestCase
 		$user->address = "54, Kilani street, Akarigbo, Jiyanland.";
 		$user->phone = "07834531265";
 		$this->assertTrue($user->save());
+
+		$this->assertEquals("Tayo", $user->name);
 	}
 	
 	public function testDeleteUser()
@@ -36,8 +39,116 @@ class UserTest extends \PHPUnit_Framework_TestCase
 		$this->assertTrue(User::destroy(1));
 	}
 
-	public function testFindUser()
+	public function testFindandUpdateUser()
 	{
-		$this->assertEquals("Opeyemiabiodun\PotatoORM\Models\User", get_class(User::find(3)));
+		$user = User::find(7);
+
+		$this->assertEquals("Opeyemiabiodun\PotatoORM\Models\User", get_class($user));
+
+		$user->address = "No. 1 Update grove, off The Past Street, Now Savedland.";
+
+		$this->assertTrue($user->save());
+	}
+
+	public function testGetAllUsers()
+	{
+		$records = User::getAll();
+
+		$count = count(User::getAll());
+
+		for ($i=0; $i < $count; $i++) { 
+			$this->assertEquals("Opeyemiabiodun\PotatoORM\Models\User", get_class($records[$i]));
+		}
+	}
+
+	/**
+     * @expectedException Opeyemiabiodun\PotatoORM\Exceptions\PropertyNotFoundException
+     */
+	public function testSetPropertyNotFoundException()
+	{
+		$user = new User();
+
+		$user->nonproperty = "faker";
+	}
+
+	/**
+     * @expectedException Opeyemiabiodun\PotatoORM\Exceptions\AssignmentException
+     */
+	public function testAssignmentException()
+	{
+		$user = new User();
+
+		$user->nonproperty = new stdClass();
+	}
+
+	/**
+     * @expectedException Opeyemiabiodun\PotatoORM\Exceptions\PropertyNotFoundException
+     */
+	public function testGetPropertyNotFoundException()
+	{
+		$user = new User();
+
+		$user->nonproperty;
+	}
+
+	/**
+     * @expectedException InvalidArgumentException
+     */
+	public function testDeleteUserInvalidArgumentExceptionI()
+	{
+		User::destroy("3");
+	}
+
+	/**
+     * @expectedException InvalidArgumentException
+     */
+	public function testDeleteUserInvalidArgumentExceptionII()
+	{
+		User::destroy(-1);
+	}
+
+	/**
+     * @expectedException InvalidArgumentException
+     */
+	public function testFindUserInvalidArgumentExceptionI()
+	{
+		User::find("3");
+	}
+
+	/**
+     * @expectedException InvalidArgumentException
+     */
+	public function testFindUserInvalidArgumentExceptionII()
+	{
+		User::find(-1);
+	}
+
+	/**
+     * @expectedException InvalidArgumentException
+     */
+	public function testSetTableInvalidArgumentException()
+	{
+		$anotheruser = new User([], null, 1234);
+	}
+
+	/**
+     * @expectedException RuntimeException
+     */
+	public function testSaveHasRuntimeException()
+	{
+		$user = new User();
+
+		$user->save();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
