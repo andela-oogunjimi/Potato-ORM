141f5b32f03f78b508786cf037b145a2f512ab46
Ogunjimi Opeyemi Abiodun <opeyemi.ogunjimi@andela.com>
2015-12-20 15:17:15 -0500

    Applied fixes from StyleCI

--- src/Connections/PgSqlConnection.php
+++ src/Connections/PgSqlConnection.php
@@ -6,9 +6,6 @@ use PDO;
 use PDOException;
 use RuntimeException;
 use InvalidArgumentException;
-use Opeyemiabiodun\PotatoORM\Connections\Connection;
-use Opeyemiabiodun\PotatoORM\Connections\LoadEnvVariablesTrait;
-use Opeyemiabiodun\PotatoORM\Connections\DatabaseTransactionsTrait;
 
 final class PgSqlConnection implements Connection
 {
@@ -20,7 +17,7 @@ final class PgSqlConnection implements Connection
      * @var PDO
      */
     private $_pdo;
-    
+
     /**
      * The method called in the constructor.
      *
@@ -88,7 +85,7 @@ final class PgSqlConnection implements Connection
                                             and ku.table_name='{$table}'
                                             ORDER BY KU.TABLE_NAME, KU.ORDINAL_POSITION;")->fetchAll();
         if (count($array) === 0) {
-            throw new RuntimeException("Error Processing Request", 1);
+            throw new RuntimeException('Error Processing Request', 1);
         }
 
         return $array[0]['primarykeycolumn'];
